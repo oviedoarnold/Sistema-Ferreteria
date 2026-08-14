9375660cc0abcdb691eddd0837ebc71eccafb72f
Arnold <60153288+Kratos9@users.noreply.github.com>
2026-08-13 19:51:05 -0600

    refactor(routes): agrega ruta para modulo de cotizaciones

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,123 +1,115 @@
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
 } from "react-router-dom"
 
 import Login from "../pages/Login"
 import Dashboard from "../pages/Dashboard"
 import Products from "../pages/Products"
 import POS from "../pages/POS"
+import Quotes from "../pages/Quotes"
 import SalesHistory from "../pages/SalesHistory"
 import Clients from "../pages/Clients"
 import Suppliers from "../pages/Suppliers"
+
 import MainLayout from "../layouts/MainLayout"
-
-
 import ProtectedRoute from "./ProtectedRoute"
 
 function AppRouter() {
-
   return (
-
     <BrowserRouter>
-
       <Routes>
-
         {/* LOGIN */}
-
         <Route
           path="/"
           element={<Login />}
         />
 
         {/* DASHBOARD */}
-
         <Route
           path="/dashboard"
           element={
             <ProtectedRoute>
-
               <MainLayout>
                 <Dashboard />
               </MainLayout>
-
             </ProtectedRoute>
           }
         />
 
         {/* PRODUCTOS */}
-
         <Route
           path="/products"
           element={
             <ProtectedRoute>
-
               <MainLayout>
                 <Products />
               </MainLayout>
-
             </ProtectedRoute>
           }
         />
+
         {/* CLIENTES */}
         <Route
-        path="/clients"
-        element={
-          <ProtectedRoute>
-            <MainLayout>
-              <Clients />
-            </MainLayout>
-          </ProtectedRoute>
-        }
-      />
+          path="/clients"
+          element={
+            <ProtectedRoute>
+              <MainLayout>
+                <Clients />
+              </MainLayout>
+            </ProtectedRoute>
+          }
+        />
 
-        {/* POS */}
-
+        {/* FACTURAR */}
         <Route
           path="/pos"
           element={
             <ProtectedRoute>
-
               <MainLayout>
                 <POS />
               </MainLayout>
-
             </ProtectedRoute>
           }
         />
 
-        {/* HISTORIAL VENTAS */}
+        {/* COTIZAR */}
+        <Route
+          path="/quotes"
+          element={
+            <ProtectedRoute>
+              <MainLayout>
+                <Quotes />
+              </MainLayout>
+            </ProtectedRoute>
+          }
+        />
 
+        {/* HISTORIAL */}
         <Route
           path="/sales-history"
           element={
             <ProtectedRoute>
-
               <MainLayout>
                 <SalesHistory />
               </MainLayout>
-
             </ProtectedRoute>
           }
-          />
-        {/* PROVEEDORES */}
+        />
 
+        {/* PROVEEDORES */}
         <Route
           path="/suppliers"
           element={
             <ProtectedRoute>
-
               <MainLayout>
                 <Suppliers />
               </MainLayout>
-
             </ProtectedRoute>
           }
         />
-     
       </Routes>
-
     </BrowserRouter>
   )
 }
